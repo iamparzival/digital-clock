2c6a6a1e6364314a1ab90b5027e1f979a3b082a0
Yash <pathakyash427@gmail.com>
2022-01-18 18:08:56 +0530

    Updates

---
+++ precache-manifest.010bee6b4745fda9c91a70f65089a390.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6561d137791c3c11fdf42323db7086de",
+    "url": "/digital-clock/index.html"
+  },
+  {
+    "revision": "a1a0636bc930e7b7e7c7",
+    "url": "/digital-clock/static/js/2.db7586e6.chunk.js"
+  },
+  {
+    "revision": "4fa01809d610d994334b",
+    "url": "/digital-clock/static/js/main.4d056edf.chunk.js"
+  },
+  {
+    "revision": "86ed1f8d15b7a228d949",
+    "url": "/digital-clock/static/js/runtime-main.fb39b3bc.js"
+  }
+]);
